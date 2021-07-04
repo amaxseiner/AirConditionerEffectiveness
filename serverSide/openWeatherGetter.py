ce9b3d05fcbcef4e1adc6c29f3806fbbd85e38aa
Alex Maxseiner <amaxsein@masonlive.gmu.edu>
2021-07-04 15:27:49 -0400

    working cloud weather getter

--- serverSide/openWeatherGetter.py
+++ serverSide/openWeatherGetter.py
@@ -6,9 +6,8 @@
 import requests, json
 
 f = open("secret.txt","r")
-
 # Enter your API key here
-api_key = f.readline()
+api_key = str(f.readline().split("\n")[0])
 
 f.close()
 
@@ -27,7 +26,6 @@ response = requests.get(complete_url)
 # convert json format data into
 # python format data
 x = response.json()
-print(x)
 # Now x contains list of nested dictionaries
 # Check the value of "cod" key is equal to
 # "404", means city is found otherwise,
@@ -40,34 +38,14 @@ if x["cod"] != "404":
 
 	# store the value corresponding
 	# to the "temp" key of y
-	current_temperature = y["temp"]
+	current_temperature = y["temp"] - 273.15 # in celcius
 
-	# store the value corresponding
-	# to the "pressure" key of y
-	current_pressure = y["pressure"]
 
 	# store the value corresponding
 	# to the "humidity" key of y
-	current_humidity = y["humidity"]
+	current_humidity = y["humidity"] # %
 
-	# store the value of "weather"
-	# key in variable z
-	z = x["weather"]
-
-	# store the value corresponding
-	# to the "description" key at
-	# the 0th index of z
-	weather_description = z[0]["description"]
-
-	# print following values
-	print(" Temperature (in kelvin unit) = " +
-					str(current_temperature) +
-		"\n atmospheric pressure (in hPa unit) = " +
-					str(current_pressure) +
-		"\n humidity (in percentage) = " +
-					str(current_humidity) +
-		"\n description = " +
-					str(weather_description))
+	print(current_temperature,current_humidity)
 
 else:
 	print(" City Not Found ")
